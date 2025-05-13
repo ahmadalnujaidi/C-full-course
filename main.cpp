f1e7500ce26d30917e93cb1503d8e23e7424233b
ahmadalnujaidi <ahmadalnujaidi02@gmail.com>
2025-05-13 22:02:12 +0300

    initial

---
+++ main.cpp
@@ -0,0 +1,8 @@
+// #include <iostream>
+//
+// int main() {
+//     std::cout << "Hello World!" <<std::endl; // end line or \n
+//     std::cout << "its really working";
+//     return 0;
+// } INTRODUCTION
+
