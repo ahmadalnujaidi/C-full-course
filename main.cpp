b3f1e1c5ce8d27a61e86ce54aab0a00ccce48270
ahmadalnujaidi <ahmadalnujaidi02@gmail.com>
2025-05-14 10:29:50 +0300

    variables and basic data types

--- main.cpp
+++ main.cpp
@@ -6,3 +6,32 @@
 //     return 0;
 // } INTRODUCTION
 
+// #include <iostream>
+// int main() {
+//     int x; // declaration
+//     x = 5; // assignment
+//     int y = 6; // both dec and assn
+//     int sum = x + y;
+//     std::cout << x << std::endl;
+//     std::cout << y << '\n';
+//     std::cout << sum << std::endl;
+//     int age = 22;
+//
+//     double price = 3.99;
+//     double gpa = 3.01;
+//     double temp = 25.1;
+//     std::cout << price << std::endl;
+//
+//     char grade = 'A';
+//     char initial = 'I';
+//     std::cout << grade << std::endl;
+//
+//     bool isStudent = true;
+//     bool power = false;
+//
+//     std::string name = "Ahmad";
+//     std::cout << name << std::endl;
+//     std::cout << "Hello " << name << std::endl;
+//     std::cout << "You are " << age << " years old";
+//     return 0;
+// } VARIABLES AND BASIC DATA TYPES
