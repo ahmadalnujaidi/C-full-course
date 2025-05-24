e94c87bf4fe88ccdbd561f4413ecc856b49d9246
ahmadalnujaidi <ahmadalnujaidi02@gmail.com>
2025-05-24 10:31:26 +0300

    hypotenuse calculator practice program

--- main.cpp
+++ main.cpp
@@ -167,3 +167,21 @@
 //
 //     return 0;
 // } USEFUL MATH RELATED FUNCTIONS
+
+// #include <iostream>
+// #include <cmath>
+// int main() {
+//     double a;
+//     double b;
+//     double c;
+//     std::cout << "Enter a number for side A: ";
+//     std::cin >> a;
+//     std::cout << "Enter a number for side B: ";
+//     std::cin >> b;
+//     // a = pow(a, 2);
+//     // b = pow(b, 2);
+//     c = sqrt(pow(a, 2) + pow(b, 2));
+//     std::cout << "side C: " << c << std::endl;
+//
+//     return 0;
+// } HYPOTENUSE CALCULATOR PRACTICE PROGRAM
